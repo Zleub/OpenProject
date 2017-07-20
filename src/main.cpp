cec9b81dcdb63be1701ac912b97636dd2240485c
Zleub <debray.arnaud@gmail.com>
2017-07-20 19:29:56 +0200

    global CMakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 //  sdddddddddddddddddddddddds   @Last modified by: adebray
 //  sdddddddddddddddddddddddds
 //  :ddddddddddhyyddddddddddd:   @Created: 2017-07-20T00:11:14+02:00
-//   odddddddd/`:-`sdddddddds    @Modified: 2017-07-20T00:11:43+02:00
+//   odddddddd/`:-`sdddddddds    @Modified: 2017-07-20T18:18:20+02:00
 //    +ddddddh`+dh +dddddddo
 //     -sdddddh///sdddddds-
 //       .+ydddddddddhs/.
@@ -15,6 +15,25 @@
 #include <demo.h>
 
 int main() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow * window = glfwCreateWindow(640, 480, "My Window", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
 	gl_version();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return (0);
 }
